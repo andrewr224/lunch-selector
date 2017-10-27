7e203cfb3f7d807f6c64b3a55a2367d366f631d8
Andrew Rozhenko <andrew.r224@gmail.com>
2017-10-27 13:16:25 +0300

    Add create, update, and destrouy action to MenusController, along with views

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -6,4 +6,33 @@ class MenusController < ApplicationController
   def show
     @menu = Menu.find(params[:id])
   end
+
+  def new
+    @menu = Menu.new
+  end
+
+  def create
+    @menu = Menu.new
+    @menu.save
+    flash.notice = 'Menu created!'
+    redirect_to menu_path(@menu)
+  end
+
+  def edit
+    @menu = Menu.find(params[:id])
+  end
+
+  def update
+    @menu = Menu.find(params[:id])
+    # no need to change the actual menu
+    flash.notice = 'Menu updated!'
+    redirect_to menu_path(@menu)
+  end
+
+  def destroy
+    @menu = Menu.find(params[:id])
+    @menu.destroy
+    flash.notice = 'Menu destroyed!'
+    redirect_to menus_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :menus
+
+  root to: 'menus#index'
 end
