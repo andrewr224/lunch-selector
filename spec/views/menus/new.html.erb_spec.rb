00b90042b6fa8b041ef0c21dd1c245c6fb5e0092
Andrew Rozhenko <andrew.r224@gmail.com>
2017-10-27 15:53:30 +0300

    Remove unneeded comment from menu/index spec

--- spec/views/menus/new.html.erb_spec.rb
+++ spec/views/menus/new.html.erb_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 RSpec.describe 'menus/new.html.erb', type: :feature do
   it 'allows to add a new menu' do
-    #assign(:menu, Menu.create(id: 1, created_at: Time.now))
     visit new_menu_path
     within 'form' do
       click_button 'Create Menu'
