cd2ca5125eca3bdd5e09b32cf78962c25b4ed291
Andrew Rozhenko <andrew.r224@gmail.com>
2017-11-02 17:04:16 +0200

    Add ordinalize to decorator

--- app/decorators/menu_decorator.rb
+++ app/decorators/menu_decorator.rb
@@ -2,7 +2,8 @@ class MenuDecorator < Draper::Decorator
   delegate_all
 
   def day_stamp
-    "#{created_at.strftime('%A')}, #{created_at.day}th of "\
+    "#{created_at.strftime('%A')}, "\
+    "#{ActiveSupport::Inflector.ordinalize(created_at.day)} of "\
     "#{created_at.strftime('%B')}"
   end
 end
