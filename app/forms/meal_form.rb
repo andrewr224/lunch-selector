f249e9221d994871fb2fd845e47a12876c432b74
Andrew Rozhenko <andrew.r224@gmail.com>
2017-11-06 14:34:14 +0200

    Add meal name length validation

--- app/forms/meal_form.rb
+++ app/forms/meal_form.rb
@@ -1,4 +1,4 @@
 class MealForm < Reform::Form
   properties :name, :course
-  validates  :name, :course, presence: true
+  validates  :name, :course, presence: true, length: { within: (3..25) }
 end
--- spec/forms/meal_form_spec.rb
+++ spec/forms/meal_form_spec.rb
@@ -9,13 +9,27 @@ RSpec.describe 'MealForm' do
     expect(form.errors[:title]).to be_empty
   end
 
+  it 'requires meal to have specified course' do
+    form.validate(params.merge(course: nil))
+    expect(form.errors[:course]).to include("can't be blank")
+  end
+
   it 'requires meal name' do
     form.validate(params.merge(name: nil))
     expect(form.errors[:name]).to include("can't be blank")
   end
 
-  it 'requires meal to have specified course' do
-    form.validate(params.merge(course: nil))
-    expect(form.errors[:course]).to include("can't be blank")
+  context 'when name is too short' do
+    it 'is not valid' do
+      form.validate(params.merge(name: 'av'))
+      expect(form.errors[:name].first).to include('too short')
+    end
+  end
+
+  context 'when name is too long' do
+    it 'is not valid' do
+      form.validate(params.merge(name: 'very long and cumbersome name'))
+      expect(form.errors[:name].first).to include('too long')
+    end
   end
 end
