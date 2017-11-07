8b9e4f2f77fb667960baa3c8dc8222e5a0b22bbc
Andrew Rozhenko <andrew.r224@gmail.com>
2017-11-07 14:19:59 +0200

    Add local variables for partials

--- app/facades/menu/show_facade.rb
+++ app/facades/menu/show_facade.rb
@@ -18,6 +18,18 @@ class Menu
       @menu ||= Menu.find_by(id: id)&.decorate
     end
 
+    def first_course
+      meals.select{ |m| m.course == "First Course" }
+    end
+
+    def main_course
+      meals.select{ |m| m.course == "Main Course" }
+    end
+
+    def beverage
+      meals.select{ |m| m.course == "Beverage" }
+    end
+
     private
 
     attr_reader :id
