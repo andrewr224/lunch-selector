fa437a7dc99bbd6979b0c6880ea9a180ef044415
Andrew Rozhenko <andrew.r224@gmail.com>
2017-11-08 17:12:46 +0200

    Fix Order feature spec

--- spec/features/orders_spec.rb
+++ spec/features/orders_spec.rb
@@ -3,8 +3,13 @@ require 'rails_helper'
 RSpec.feature 'Orders', type: :feature do
   let(:menu) { create(:menu) }
 
+  before do
+    menu.meals.create(name: 'Pasta', course: 'Main Course')
+    menu.meals.create(name: 'Tea', course: 'Beverage')
+  end
+
   describe 'adding orders' do
-    scenario 'creates a new meal' do
+    scenario 'creates a new order' do
       visit menu_path menu
       within('.new_order') do
         click_button 'Submit'
