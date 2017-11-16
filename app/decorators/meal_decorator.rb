7787cad3f962377dcd455864af4bf52a350fd864
Andrew Rozhenko <andrew.r224@gmail.com>
2017-11-16 16:45:43 +0200

    Fix MealDecorator#todays_price

--- app/decorators/meal_decorator.rb
+++ app/decorators/meal_decorator.rb
@@ -2,6 +2,6 @@ class MealDecorator < Draper::Decorator
   delegate_all
 
   def todays_price(menu)
-    meal&.menu_items.find_by(menu_id: menu.id).price
+    meal.menu_items.find_by(menu_id: menu.id)&.price
   end
 end
