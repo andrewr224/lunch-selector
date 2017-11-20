4f1a4060bd238670e4efb2ffd683927a8ad08a62
Andrew Rozhenko <andrew.r224@gmail.com>
2017-11-20 14:59:44 +0200

    Remove complex logic from views

--- app/decorators/menu_decorator.rb
+++ app/decorators/menu_decorator.rb
@@ -5,4 +5,12 @@ class MenuDecorator < Draper::Decorator
     "#{created_at.strftime('%A')}, #{created_at.day.ordinalize} of "\
     "#{created_at.strftime('%B')}"
   end
+
+  def placed_orer?(current_user)
+    current_user.orders.find_by(menu_id: menu.id)
+  end
+
+  def show_price?(current_user)
+    current_user.admin? && menu.orders.any?
+  end
 end
